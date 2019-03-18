b639b011b8d8c603e08fdcc360b81720609384dd
Rengarajan.Kannan <rengarajan.kannan@cognizant.com>
2019-03-17 21:57:09 -0400

    First Commit

---
+++ src/main/java/com/loriini/LoriiniServicesApplication.java
@@ -0,0 +1,13 @@
+package com.loriini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoriiniServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoriiniServicesApplication.class, args);
+	}
+
+}
