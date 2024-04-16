d7e42126aa7073244b7cd3a8487555ce5571e74c
renga4599 <32118579+renga4599@users.noreply.github.com>
2024-04-16 14:01:32 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+fpr, tpr, thresholds = roc_curve(y_test, y_scores)
+roc_auc = auc(fpr, tpr)
+
+# Plotting
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve for Random Forest Classifier')
+plt.legend(loc="lower right")
+plt.show()
+
